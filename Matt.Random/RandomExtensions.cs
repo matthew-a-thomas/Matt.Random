e5c1d06dfdfe3f601f8ce4e48748569e5d115811
Matthew Thomas <me@matthewathomas.com>
2017-12-14 10:26:09 -0500

    Added ToEndlessBitSequence with test

--- Matt.Random/RandomExtensions.cs
+++ Matt.Random/RandomExtensions.cs
@@ -1,27 +1,30 @@
 ﻿namespace Matt.Random
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Security.Cryptography;
     using Adapters;
+    using Bits;
 
-    [SuppressMessage("ReSharper",
-        "UnusedMember.Global")]
     public static class RandomExtensions
     {
         /// <summary>
         /// Adapts this <see cref="Random"/> into an <see cref="IRandom"/>.
         /// </summary>
+        // ReSharper disable once UnusedMember.Global
         public static IRandom AsRandom(Random random) => new RandomAdapter(random);
         
         /// <summary>
         /// Adapts this <see cref="RandomNumberGenerator"/> into an <see cref="IRandom"/>.
         /// </summary>
+        // ReSharper disable once UnusedMember.Global
         public static IRandom AsRandom(RandomNumberGenerator rng) => new RandomNumberGeneratorAdapter(rng);
         
         /// <summary>
         /// Populates the entire array with random bytes.
         /// </summary>
+        // ReSharper disable once MemberCanBePrivate.Global
         public static void Populate(
             this IRandom random,
             byte[] buffer) =>
@@ -29,5 +32,20 @@
                 buffer,
                 0,
                 buffer.Length);
+        
+        /// <summary>
+        /// Produces an endless sequence of random bits from this <see cref="IRandom"/>.
+        /// </summary>
+        [SuppressMessage("ReSharper", "IteratorNeverReturns")]
+        public static IEnumerable<bool> ToEndlessBitSequence(this IRandom random)
+        {
+            var buffer = new byte[4];
+            while (true)
+            {
+                random.Populate(buffer);
+                foreach (var bit in buffer.ToBits())
+                    yield return bit;
+            }
+        }
     }
 }
---
+++ Tests/RandomExtensionsClass.cs
@@ -0,0 +1,28 @@
+﻿namespace Tests
+{
+    using System.Linq;
+    using Matt.Random;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class RandomExtensionsClass
+    {
+        [TestClass]
+        public class ToEndlessBitSequenceMethod
+        {
+            [TestMethod]
+            public void InvokesRandomPopulate()
+            {
+                var mocked = new Mock<IRandom>();
+                mocked
+                    .Object
+                    .ToEndlessBitSequence()
+                    .Take(10)
+                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
+                    .ToList();
+                mocked.Verify(x => x.Populate(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>()));
+            }
+        }
+    }
+}
